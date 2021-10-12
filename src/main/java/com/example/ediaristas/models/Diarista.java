6854a751983be2576d1b3ab91a8e190ddb9fead3
wslmacieira <wslmacieira@outlook.com>
2021-10-11 23:57:00 -0300

    Cria o modelo Diarista

---
+++ src/main/java/com/example/ediaristas/models/Diarista.java
@@ -0,0 +1,61 @@
+package com.example.ediaristas.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Diarista {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nomeCompleto;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 11)
+    private String telefone;
+
+    @Column(nullable = false)
+    private String logradouro;
+
+    @Column(nullable = false)
+    private String numero;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = true)
+    private String complemento;
+
+    @Column(nullable = false, length = 8)
+    private String cep;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(nullable = false, length = 2)
+    private String estado;
+
+    @Column(nullable = false)
+    private String codigoIbge;
+}
