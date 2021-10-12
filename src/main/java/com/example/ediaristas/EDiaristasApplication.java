81ebfe6073c48f9c0edc6d9458463b0fbc08080b
wslmacieira <wslmacieira@outlook.com>
2021-10-11 23:36:11 -0300

    Inicializando a aplicação

---
+++ src/main/java/com/example/ediaristas/EDiaristasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ediaristas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDiaristasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDiaristasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ediaristas/EDiaristasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ediaristas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDiaristasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
