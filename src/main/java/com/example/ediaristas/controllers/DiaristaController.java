f48a3d57aeadeb91e8ee5e9a821e2a16ffc3d3c4
wslmacieira <wslmacieira@outlook.com>
2021-10-12 08:57:32 -0300

    Cria action de cadastro de diarista

--- src/main/java/com/example/ediaristas/controllers/DiaristaController.java
+++ src/main/java/com/example/ediaristas/controllers/DiaristaController.java
@@ -1,15 +1,21 @@
 package com.example.ediaristas.controllers;
 
 import com.example.ediaristas.models.Diarista;
+import com.example.ediaristas.reposiries.DiaristaRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/admin/diaristas")
 public class DiaristaController {
+
+    @Autowired
+    private DiaristaRepository repository;
     
     @GetMapping("/cadastrar")
     public ModelAndView cadastrar() {
@@ -18,5 +24,12 @@ public class DiaristaController {
         modelAndView.addObject("diarista", new Diarista());
 
         return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(Diarista diarista) {
+        repository.save(diarista);
+
+        return "redirect:/admin/diaristas/cadastrar";
     }
 }
