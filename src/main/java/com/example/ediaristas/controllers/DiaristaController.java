9fa5b1fba1d41007d73631d2486e1069c54333b9
wslmacieira <wslmacieira@outlook.com>
2021-10-13 00:34:46 -0300

    Implementa validação com Bean Validation

--- src/main/java/com/example/ediaristas/controllers/DiaristaController.java
+++ src/main/java/com/example/ediaristas/controllers/DiaristaController.java
@@ -1,10 +1,13 @@
 package com.example.ediaristas.controllers;
 
+import javax.validation.Valid;
+
 import com.example.ediaristas.models.Diarista;
 import com.example.ediaristas.reposiries.DiaristaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,7 +40,29 @@ public class DiaristaController {
     }
 
     @PostMapping("/cadastrar")
-    public String cadastrar(Diarista diarista) {
+    public String cadastrar(@Valid Diarista diarista, BindingResult result) {
+        if (result.hasErrors()) {
+            return "admin/diaristas/form";
+        }
+        repository.save(diarista);
+
+        return "redirect:/admin/diaristas";
+    }
+
+    @GetMapping("/{id}/editar")
+    public ModelAndView editar(@PathVariable Long id) {
+        var modelAndView = new ModelAndView("admin/diaristas/form");
+
+        modelAndView.addObject("diarista", repository.getById(id));
+
+        return modelAndView;
+    }
+
+    @PostMapping("/{id}/editar")
+    public String editar(@PathVariable Long id, @Valid Diarista diarista, BindingResult result) {
+        if (result.hasErrors()) {
+            return "admin/diaristas/form";
+        }
         repository.save(diarista);
 
         return "redirect:/admin/diaristas";
@@ -46,7 +71,7 @@ public class DiaristaController {
     @GetMapping("/{id}/excluir")
     public String excluir(@PathVariable Long id) {
         repository.deleteById(id);
-        
+
         return "redirect:/admin/diaristas";
     }
 }
--- src/main/java/com/example/ediaristas/models/Diarista.java
+++ src/main/java/com/example/ediaristas/models/Diarista.java
@@ -5,6 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CPF;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,39 +29,63 @@ public class Diarista {
     @EqualsAndHashCode.Include
     private Long id;
 
+    @NotNull
+    @Size(min = 3, max = 100)
     @Column(nullable = false, length = 100)
     private String nomeCompleto;
 
+    @NotNull
+    @Size(min = 11, max = 14)
+    @CPF
     @Column(nullable = false, unique = true, length = 11)
     private String cpf;
 
+    @NotNull
+    @NotEmpty
+    @Email
     @Column(nullable = false, unique = true)
     private String email;
 
+    @NotNull
+    @Size(min = 11, max = 14)
     @Column(nullable = false, length = 11)
     private String telefone;
 
+    @NotNull
+    @NotEmpty
     @Column(nullable = false)
     private String logradouro;
 
+    @NotNull
+    @NotEmpty
     @Column(nullable = false)
     private String numero;
 
+    @NotNull
+    @NotEmpty
     @Column(nullable = false)
     private String bairro;
 
     @Column(nullable = true)
     private String complemento;
 
+    @NotNull
+    @Size(min = 8, max = 9)
     @Column(nullable = false, length = 8)
     private String cep;
 
+    @NotNull
+    @NotEmpty
     @Column(nullable = false)
     private String cidade;
 
+    @NotNull
+    @Size(min = 2, max = 2)
     @Column(nullable = false, length = 2)
     private String estado;
 
+    @NotNull
+    @NotEmpty
     @Column(nullable = false)
     private String codigoIbge;
 }
