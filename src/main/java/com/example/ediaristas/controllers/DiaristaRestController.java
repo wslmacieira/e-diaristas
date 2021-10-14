f22a4f62a01abbf220e09c6fc5a336f0c6e9cbe5
wslmacieira <wslmacieira@outlook.com>
2021-10-14 12:42:27 -0300

    Filtra diarista com base no código IBGE

--- src/main/java/com/example/ediaristas/controllers/DiaristaRestController.java
+++ src/main/java/com/example/ediaristas/controllers/DiaristaRestController.java
@@ -2,12 +2,15 @@ package com.example.ediaristas.controllers;
 
 import java.util.List;
 
+import com.example.ediaristas.dtos.ViaCepResponse;
 import com.example.ediaristas.models.Diarista;
 import com.example.ediaristas.repositories.DiaristaRepository;
+import com.example.ediaristas.services.ViaCepService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +20,13 @@ public class DiaristaRestController {
     @Autowired
     private DiaristaRepository repository;
 
+    @Autowired
+    private ViaCepService viaCepService;
+
     @GetMapping
-    public List<Diarista> buscarDiaristaPorCep() {
-        return repository.findAll();
+    public List<Diarista> buscarDiaristaPorCep(@RequestParam String cep) {
+        var endereco = viaCepService.buscarEnderecoPorCep(cep);
+        var codigoIbge = endereco.getIbge();
+        return repository.findByCodigoIbge(codigoIbge);
     }
 }
--- src/main/java/com/example/ediaristas/repositories/DiaristaRepository.java
+++ src/main/java/com/example/ediaristas/repositories/DiaristaRepository.java
@@ -1,9 +1,12 @@
 package com.example.ediaristas.repositories;
 
+import java.util.List;
+
 import com.example.ediaristas.models.Diarista;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DiaristaRepository extends JpaRepository<Diarista, Long>{
     
+    List<Diarista> findByCodigoIbge(String codigoIbge);
 }
